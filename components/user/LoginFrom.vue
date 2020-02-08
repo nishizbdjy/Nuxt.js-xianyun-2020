2a26520d2897f23ec538757f6cbfd6a0eeebe65d
lbw <2659154921@qq.com>
2020-02-08 16:24:45 +0800

    完成form表单用户点击登陆

--- components/user/LoginFrom.vue
+++ components/user/LoginFrom.vue
@@ -1,11 +1,11 @@
 <template>
   <el-form :model="form" ref="form" :rules="rules" class="form">
     <el-form-item class="form-item" prop="username">
-      <el-input placeholder="用户名/手机"></el-input>
+      <el-input placeholder="用户名/手机" v-model="form.username"></el-input>
     </el-form-item>
 
     <el-form-item class="form-item" prop="password">
-      <el-input placeholder="密码" type="password"></el-input>
+      <el-input placeholder="密码" type="password" v-model="form.password"></el-input>
     </el-form-item>
 
     <p class="form-text">
@@ -22,19 +22,37 @@ export default {
     return {
       // 表单数据
       form: {
-          username:'',
-          password:''
+        username: "",
+        password: ""
       },
       // 表单规则
       rules: {
-        
+        username: [
+          { required: true, message: "请输入用户名", trigger: "blur" }
+        ],
+
+        password: [
+          {
+            required: true,
+            message: "请输入密码",
+            trigger: "blur"
+          }
+        ]
       }
     };
   },
   methods: {
     // 提交登录
     handleLoginSubmit() {
-      console.log(this.form);
+      // console.log(this.form);
+      this.$refs.form.validate(valid => {
+        //如果成功
+        if (valid) {
+          
+        } else {
+          this.$message("数据不合法");
+        }
+      });
     }
   }
 };

f23e01d31038ca46bfe5052c62af6a02e4c2c787
lbw <2659154921@qq.com>
2020-02-12 17:33:33 +0800

    完成相隔时间的计算

--- components/air/flightsItem.vue
+++ components/air/flightsItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flight-item">
-    <div   @click="inShow=!inShow">
+    <div @click="inShow=!inShow">
       <!-- 显示的机票信息 -->
       <el-row type="flex" align="middle" class="flight-info">
         <el-col :span="6">
@@ -14,7 +14,7 @@
               <span>{{data.org_airport_name}} {{data.org_airport_quay}}</span>
             </el-col>
             <el-col :span="8" class="flight-time">
-              <span>2时20分</span>
+              <span>{{xianggeDate}}</span>
             </el-col>
             <el-col :span="8" class="flight-airport">
               <strong>{{data.arr_time}}</strong>
@@ -72,6 +72,31 @@ export default {
       //显示子列表的
       inShow: false
     };
+  },
+  computed: {
+    //添加相隔时间的计算
+    xianggeDate() {
+      //到达时间
+      let arr = this.data.arr_time;
+      //出发事件
+      let dep = this.data.dep_time;
+      //到达时间数组
+      let end = arr.split(":");
+      //出发事件的数组
+      let start = dep.split(":");
+      //判断出发时间大于到达时间
+      if (start[0] > end[0]) {
+        end[0] += 24;
+      }
+      //相减的整个值，分钟       隐式转换
+      const dis = end[0] * 60 + +end[1] - start[0] * 60 + +start[1];
+      //小时         向下取整
+      const hours = Math.floor(dis / 60);
+      //分钟  取余数
+      const min = dis % 60;
+      //拼接
+      return `${hours}时${min}分`;
+    }
   }
 };
 </script>
